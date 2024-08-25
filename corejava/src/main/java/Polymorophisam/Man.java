d73a4c674bc468b69ccebad82d138264886d7822
achyu <achyutpatewar@gmail.com>
2024-08-25 12:30:18 +0530

    first commit

---
+++ corejava/src/main/java/Polymorophisam/Man.java
@@ -0,0 +1,21 @@
+package Polymorophisam;
+
+abstract class employee{
+    abstract void WorkingAsEmployee();
+}
+
+
+class child extends employee {
+
+
+    @Override
+    void WorkingAsEmployee() {
+
+    }
+}
+
+public class Man {
+    public static void main(String[] args) {
+
+    }
+}
